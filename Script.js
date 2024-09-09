9e11401b6ec91b9168c30575bef3accf912c435b
sunita <sunitaraut623@gmail.com>
2024-09-09 23:09:45 +0545

    added some dummy data

---
+++ Script.js
@@ -0,0 +1,12 @@
+const quotes = {
+    science: [
+        "Science is the key to our future, and if you don’t believe in science, then you’re holding everybody back. – Bill Nye",
+        "The important thing is not to stop questioning. Curiosity has its own reason for existing. – Albert Einstein",
+        "Somewhere, something incredible is waiting to be known. – Carl Sagan"
+    ],
+    inspiration: [
+        "The only way to do great work is to love what you do. – Steve Jobs",
+        "Your time is limited, don't waste it living someone else's life. – Steve Jobs",
+        "Don't watch the clock; do what it does. Keep going. – Sam Levenson"
+    ]
+};
